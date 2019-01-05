33488795856d939b5e70de5a6c918f1e3569955e
vijayst <vijayst@gmail.com>
2019-01-05 10:05:57 +0530

    basic styling

--- Layout.js
+++ Layout.js
@@ -3,12 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
+import Menu from './Menu';
 
 export default function Layout() {
   return (
     <div className="app">
-      <div className="app__sidebar" />
-      <main>
+      <div className="app__sidebar">
+      </div>
+      <main className="app__content">
         <Switch>
           <Route path="/about" component={About} />
           <Route path="/contact" component={Contact} />
---
+++ Menu.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Menu() {
+    return (
+        <ul className="menu">
+            <li></li>
+        </ul>
+    );
+}
